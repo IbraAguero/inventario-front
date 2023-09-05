8b026a717e81f62a98a297cafb41d9825252468c
IbraAguero <ibrahimaguero@gmail.com>
2023-09-05 12:20:35 -0300

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -12,14 +12,16 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <SnackbarProvider autoHideDuration={3000} preventDuplicate>
-          <App />
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
         </SnackbarProvider>
       </BrowserRouter>
     </Provider>
